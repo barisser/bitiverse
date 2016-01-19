843e1b5cabc9fd0b8854765c592610d30d1f069a
Andrew Barisser <barisser@gmail.com>
2016-01-18 21:44:01 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,49 @@
+A finite space has been defined and will be governed on the Bitcoin Blockchain.
+The pixels can be owned and transacted via Bitcoin private keys.  Ownership is
+cryptographically self-evident as it is with bitcoins.  Pixels may be owned and
+transacted freely and without censorship.
+
+The owner of pixels within the Bitiverse may publish content at any time.  They
+may point a content link at their pixels using a 'Content Transaction'.  This
+publishes a URL to the blockchain from an address that owns pixels.  The content
+at this URL indicates what lives in these pixels, within certain constraints.
+
+Pixels are transacted via 'Transfer Transactions'.  These are also Bitcoin
+transactions that signal the transfer of pixels within a certain zone.  Naturally
+one may only transmit pixels that one already owns; this is verifiable from
+the Blockchain history.
+
+The Bitiverse is defined as a 1920x1080 pixel grid.  It shall remain so for all
+time, with no expansion or diminishment.
+
+The contents of the Bitiverse are whatever the protocol defines it as from the
+Blockchain record.  The underlying content cannot be censored, even as its
+expression may be censored by whichever medium carries it.  
+
+PROTOCOL RULES
+
+Pixels are tied to particular unspent Bitcoin outputs.  If these are transmitted
+accidentally as part of normal Bitcoin transactions, the pixels are not destroyed, but
+their ownership is transplanted wholly to the new bitcoins' owner.
+
+TRANSFER Transactions
+
+CONTENT Transactions
+
+1920x1080
+
+Each pixel is for sale all the time
+pixels are owned by addresses through blockchain tokens
+pixel contents are described via opreturn statements
+
+selling pixels
+atomic transaction between addresses
+exchange tokens for bitcoins
+opreturn statement says
+  - pixel coordinates
+
+
+defining content of pixels
+  - pixel coordinates
+  - hash of link contents
+  - link with contents for behavior (rendered html)
---
+++ pixelwriter.py
@@ -0,0 +1,84 @@
+import requests
+import messaging
+
+requests.packages.urllib3.disable_warnings()
+
+version = 0
+universe_width = 1920
+universe_height = 1080
+OP_RETURN_MAX_LENGTH = 80
+
+def compress_coords(coords_set):
+    n = 0
+    m = 1
+    for x in coords_set:
+        for y in x:
+            n += y * m
+            m = m * (universe_width)
+    return n
+
+def decompress_coords(n):
+    d = []
+    odd = True
+    r = []
+    while n > 0:
+        a = n % (universe_width)
+        n = n - a
+        n = n / (universe_width)
+        if odd:
+            r = [a]
+            odd = False
+        else:
+            r.append(a)
+            d.append(r)
+            odd = True
+            r = []
+    assert len(d) % 2 == 0
+    return d
+
+def transfer_message(coords_set):
+    return str(compress_coords(coords_set))
+
+def write_transfer_tx(from_address, coords_set, destination, private_key,
+                      predecessor_inputs, push=False, fee=messaging.default_fee, sign=True):
+    message = transfer_message(coords_set)
+
+    sum_in = 0
+    for x in predecessor_inputs:
+        sum_in += x['value']
+    amounts_array = []
+    amounts_array.append(messaging.dust) # satoshi for recipient
+    amounts_array.append(messaging.dust) # satoshi for change for me
+    amounts_array.append(sum_in - messaging.dust * 2 - fee)
+    destinations_array = []
+    destinations_array.append(destination)
+    destinations_array.append(from_address)
+    destinations_array.append(from_address)
+
+    tx = messaging.make_raw_transaction_from_specific_inputs_arrays(from_address,
+                amounts_array, destinations_array, predecessor_inputs, fee=fee)
+    tx = messaging.add_op_return(tx, message)
+    if sign:
+        tx = messaging.sign_tx(tx, private_key)
+    if push and sign:
+        tx_hash = messaging.pushtx(tx)
+        return tx_hash
+    else:
+        return tx
+
+def content_message(coords_set, content_url):
+    d = "C/"+str(compress_coords(coords_set)) + "/" + str(content_url)
+    assert len(d) <= OP_RETURN_MAX_LENGTH
+    return d
+
+def content_tx(from_address, coords_set, content_url, private_key, avoid_inputs=[], push=False):
+    message = content_message(coords_set, content_url)
+    inputs = [x for x in messaging.unspents(from_address) if not x['output'] in avoid_inputs]
+    destination = from_address
+    tx = messaging.make_unsigned_op_return_tx_with_specific_inputs(from_address, destination, message, inputs)
+    tx = messaging.sign_tx(tx, private_key)
+    if push:
+        txhash = messaging.pushtx(tx)
+        return txhash
+    else:
+        return tx
