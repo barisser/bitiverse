3b4a828a29385c6785b00426a194f8a51ead182e
barisser <barisser@gmail.com>
2016-02-14 21:00:49 -0800

    pipify, cleanup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='bitiverse',
+      version='0.1',
+      description='A Decentralized Web Page',
+      url='www.bitiverse.com',
+      author='Andrew Barisser',
+      license='MIT',
+      packages=['bitiverse'],
+      zip_safe=False)
